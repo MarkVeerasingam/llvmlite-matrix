c999a10138858a78c3d057b8d84d118a49189242
Mark Veerasinagm <markveer70@gmail.com>
2025-08-01 12:16:27 +0100

    .

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from jit_engine.engine import run_jit_vector_mul
 # project entry point. It calls to the function run_jit_vector_mul() within the jit_engine/engine.py file
 # you have creative liberity on how you want to expand it but I would personally just try and get
 # run_jit_vector_add() run_jit_vector_sub() etc... 
+
 if __name__ == "__main__":
     run_jit_vector_mul()
